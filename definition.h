858af227edae5e4b9bde575c4591684916ff58c0
hoanglamtung261106 <hoanglamtung2025@gmail.com>
2025-04-12 11:36:24 +0700

    Update definition.h

--- definition.h
+++ definition.h
@@ -1,20 +1,13 @@
 #ifndef _DEFS__H
 #define _DEFS__H
 
-const int SCREEN_WIDTH = 800;
-const int SCREEN_HEIGHT = 600;
-const char* WINDOW_TITLE = "Hello World!";
+const int SCREEN_WIDTH = 800; //Chiều rộng của screen là 800
+const int SCREEN_HEIGHT = 600; //Chiều cao của screen là 600
+const char* WINDOW_TITLE = "Hello World!"; //Tiêu đề
 
-const char* MAN_SPRITE_FILE = "foo.png";
-const int MAN_CLIPS[][4] = {
-    {  0, 0, 64, 205},
-    { 64, 0, 64, 205},
-    {128, 0, 64, 205},
-    {192, 0, 64, 205}};
-const int MAN_FRAMES = sizeof(MAN_CLIPS)/sizeof(int)/4;
-
-const char*  BIRD_SPRITE_FILE = "83127-sprite-area-line-animated-bird-film.png";
-const int BIRD_CLIPS[][4] = {
+const char* ROUND_BUTTON = "button-155149_1280.png";
+const char* BIRD_SPRITE_FILE = "83127-sprite-area-line-animated-bird-film.png"; //Chim xuôi
+const int BIRD_CLIPS[][4] = { //BIRD_CLIPS[i] là một đoạn của BIRD_SPRITE_FILE, có 4 chỉ số theo thứ tự là x, y, w, h
     {0, 0, 182, 168},
     {181, 0, 182, 168},
     {364, 0, 182, 168},
@@ -32,10 +25,10 @@ const int BIRD_CLIPS[][4] = {
     {364, 340, 182, 168},
     {547, 340, 182, 168},
 };
-const int BIRD_FRAMES = sizeof(BIRD_CLIPS)/sizeof(int)/4;
+const int BIRD_FRAMES = sizeof(BIRD_CLIPS)/sizeof(int)/4; //Ảnh chim xuôi chia ra 14 cảnh
 
-const char*  BIRD2_SPRITE_FILE = "83127-sprite-area-line-animated-bird-film (2).png";
-const int BIRD2_CLIPS[][4] = {
+const char* BIRD2_SPRITE_FILE = "83127-sprite-area-line-animated-bird-film (2).png"; //Chim ngược
+const int BIRD2_CLIPS[][4] = { //Định nghĩa tương tự cho BIRD2_SPRITE_FILE
     {0, 0, 182, 168},
     {181, 0, 182, 168},
     {364, 0, 182, 168},
@@ -53,6 +46,6 @@ const int BIRD2_CLIPS[][4] = {
     {364, 340, 182, 168},
     {547, 340, 182, 168},
 };
-const int BIRD2_FRAMES = sizeof(BIRD2_CLIPS)/sizeof(int)/4;
+const int BIRD2_FRAMES = sizeof(BIRD2_CLIPS)/sizeof(int)/4; //Ảnh chim ngược chia ra 14 cảnh
 
 #endif
